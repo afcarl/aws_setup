962ba7174849becc4760c0225b7c4666fa7dda1d
kyle-dorman <ked65@cornell.edu>
2017-01-29 14:43:07 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a set up scripts to make it easier to work with AWS EC2 while working on data projects for the
+[Udacity self-driving-car nanodegree](https://www.udacity.com/drive). These files were originally sourced
+from a fellow CarND student, Alex Staravoitau's [blog post](http://navoshta.com/aws-tensorflow/). I've
+altered the scripts slightly to work with the amazon base ami and to hide and private information so the
+files are commitable. 
+
+ 
---
+++ aws_jupyter_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source ./aws_private
+# Loads AWS_JUPYTER_HASHED_PWD 
+
+CERTIFICATE_DIR="~/certificate"
+JUPYTER_CONFIG_DIR="~/.jupyter-test"
+DEFAULT_PORT="8888"
+
+if [ ! -d "$CERTIFICATE_DIR" ]; then
+    mkdir $CERTIFICATE_DIR
+    openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout "$CERTIFICATE_DIR/mykey.key" -out "$CERTIFICATE_DIR/mycert.pem" -batch
+    chown -R ec2-user $CERTIFICATE_DIR
+fi
+
+if [ ! -f "$JUPYTER_CONFIG_DIR/jupyter_notebook_config.py" ]; then
+    # generate default config file
+    #jupyter notebook --generate-config
+    mkdir $JUPYTER_CONFIG_DIR
+
+    # append notebook server settings
+    cat <<EOF >> "$JUPYTER_CONFIG_DIR/jupyter_notebook_config.py"
+# Set options for certfile, ip, password, and toggle off browser auto-opening
+c.NotebookApp.certfile = u'$CERTIFICATE_DIR/mycert.pem'
+c.NotebookApp.keyfile = u'$CERTIFICATE_DIR/mykey.key'
+# Set ip to '*' to bind on all interfaces (ips) for the public server
+c.NotebookApp.ip = '*'
+c.NotebookApp.password = u'$AWS_JUPYTER_HASHED_PWD'
+c.NotebookApp.open_browser = False
+# It is a good idea to set a known, fixed port for server access
+c.NotebookApp.port = $DEFAULT_PORT
+EOF
+    chown -R ec2-user $JUPYTER_CONFIG_DIR
+fi
+
