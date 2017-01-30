1cd51180c473ace8590c8af78f83503068d1f526
kyle-dorman <ked65@cornell.edu>
2017-01-29 22:58:57 -0500

    Exploring how to terminate instances using python.

---
+++ terminate_ec2.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+import boto3
+
+# How to terminate an ec2 instance in python
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+ids = [i.id for i in instances]
+ec2.instances.filter(InstanceIds=ids).terminate()
